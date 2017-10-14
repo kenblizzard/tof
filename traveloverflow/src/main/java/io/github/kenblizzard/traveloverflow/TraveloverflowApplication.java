36f5f181fb13ce55afa43aa94a6e06f4330cf6e3
kenblizzard <kenneth.bolico@gmail.com>
2017-10-14 20:24:07 +0800

    added server src code

---
+++ traveloverflow/src/main/java/io/github/kenblizzard/traveloverflow/TraveloverflowApplication.java
@@ -0,0 +1,12 @@
+package io.github.kenblizzard.traveloverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveloverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveloverflowApplication.class, args);
+	}
+}
---
+++ traveloverflow/src/main/java/io/github/kenblizzard/traveloverflow/controller/IndexController.java
@@ -0,0 +1,8 @@
+package io.github.kenblizzard.traveloverflow.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+}
